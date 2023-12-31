89ed64187bb6cc8f0efe3cada9bb7f54569fe24a
youswim <youswim96@gmail.com>
2023-12-31 21:26:51 +0900

    state의 숫자만큼 네모 만들기

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import React from 'react'
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      count: 3,
+    };
   }
 
   render() {
+    const nemo_count = Array.from({length: this.state.count}, (v, i) => (i))
+    console.log(nemo_count)
     return (
       <div className='App'>
-        <div style={{
+        {nemo_count.map((num, idx) => {
+        return (
+        <div key={idx} style={{
           width:'150px',
           height:'150px',
           backgroundColor:'#eee',
@@ -17,6 +23,8 @@ class App extends React.Component {
         }}>
           nemo
         </div>
+        )})}
+        
       </div>
     )
   }
