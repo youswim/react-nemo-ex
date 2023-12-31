de5a4c1f67e48327a37b8bb849657333c3bcabaa
youswim <youswim96@gmail.com>
2023-12-31 21:58:24 +0900

    Nemo 추가 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -8,6 +8,20 @@ class App extends React.Component {
     };
   }
 
+  addNemo = () => {
+    this.setState({count: this.state.count + 1});
+    console.log("add nemo");
+  }
+  
+  removeNemo = () => {
+    if(this.state.count <= 0 ) {
+      window.alert("네모가 없어요");
+      return;
+    }
+    this.setState({count: this.state.count - 1});
+    console.log("remove nemo");
+  }
+
   render() {
     const nemo_count = Array.from({ length: this.state.count }, (v, i) => i);
     console.log(nemo_count);
@@ -20,6 +34,8 @@ class App extends React.Component {
             </div>
           );
         })}
+        <button onClick={this.addNemo}>하나 추가</button>
+        <button onClick={this.removeNemo}>하나 빼기</button>
       </div>
     );
   }
