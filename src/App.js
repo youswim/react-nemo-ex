f691b1e5eb6fff46a6b30ac9b5b24dad5ac8d14f
youswim <youswim96@gmail.com>
2024-01-01 15:23:51 +0900

    네모 만들기 기능을 리액트 ref로 리팩터링

--- src/App.js
+++ src/App.js
@@ -5,29 +5,30 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 3,
     };
+    this.div = React.createRef();
   }
 
-  addNemo = () => {
-    this.setState({count: this.state.count + 1});
-    console.log("add nemo");
+  hoverEvent = (e) => {
+    console.log(e);
+    console.log(e.target);
+
+    e.target.style.background = "#eee"
   }
-  
-  removeNemo = () => {
-    if(this.state.count <= 0 ) {
-      window.alert("네모가 없어요");
-      return;
-    }
-    this.setState({count: this.state.count - 1});
-    console.log("remove nemo");
+
+  componentDidMount(){
+    this.div.current.addEventListener("mouseover", this.hoverEvent)
+  }
+
+  componentWillUnmount() {
+    this.div.current.removeEventListener("mouseover", this.hoverEvent);
   }
 
   render() {
     const nemo_count = Array.from({ length: this.state.count }, (v, i) => i);
     console.log(nemo_count);
     return (
-      <div className="App">
+      <div className="App" ref={this.div}>
         <Nemo/>
       </div>
     );
